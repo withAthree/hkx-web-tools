4c4470cc294a47ab89475ceadfae8f421544eb71
withAthree <huangkaixiang8462@163.com>
2025-08-13 17:31:12 +0800

    feat: 更新stylelint配置

--- packages/lint/stylelint-config/__tests__/index.test.js
+++ packages/lint/stylelint-config/__tests__/index.test.js
@@ -1,4 +1,24 @@
 import { it } from 'vitest';
 
+import stylelint from 'stylelint';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-it('validate default', async () => {})
+const filename = fileURLToPath(import.meta.url);
+const rootDirname = dirname(filename);
+
+it('validate default', async () => {
+  const filePaths = [path.join(rootDirname, './fixture/test.vue')];
+
+  const result = await stylelint.lint({
+    configFile: path.join(rootDirname, '../index.js'),
+    files: filePaths,
+    fix: false,
+  });
+
+  const filesResult = JSON.parse(result.output || '[]') || [];
+  filesResult.forEach((fileResult) => {
+    console.log(`========= ${filePaths} ==========`);
+    console.log(fileResult.warnings);
+  });
+});
--- packages/lint/stylelint-config/index.js
+++ packages/lint/stylelint-config/index.js
@@ -1,45 +1,46 @@
 module.exports = {
   defaultSeverity: 'warning',
+  plugins: ['stylelint-scss'],
   rules: {
     // 不允许未知的 @规则
-    'at-rule-no-unknown': null, 
+    'at-rule-no-unknown': null,
     // 禁止空块
-    'block-no-empty': true, 
+    'block-no-empty': true,
     // 禁止使用无效的十六进制颜色
-    'color-no-invalid-hex': true, 
+    'color-no-invalid-hex': true,
     // 禁止空注释
-    'comment-no-empty': true, 
+    'comment-no-empty': true,
     // 在声明块中不允许重复属性，忽略值不同的重复属性
     'declaration-block-no-duplicate-properties': [
       true,
       {
         ignore: ['consecutive-duplicates-with-different-values'],
       },
-    ], 
+    ],
     // 禁止覆盖相关长手属性的简写属性
-    'declaration-block-no-shorthand-property-overrides': true, 
+    'declaration-block-no-shorthand-property-overrides': true,
     // 不允许在font-family中使用重复的名称
-    'font-family-no-duplicate-names': true, 
+    'font-family-no-duplicate-names': true,
     // 不允许在数学函数（如 calc() 或 min()）中使用无效的无空格运算符
-    'function-calc-no-unspaced-operator': true, 
+    'function-calc-no-unspaced-operator': true,
     // 禁止在线性渐变函数中使用非标准方向值
-    'function-linear-gradient-no-nonstandard-direction': true, 
+    'function-linear-gradient-no-nonstandard-direction': true,
     // 不允许在关键帧声明中使用无效的 !important
-    'keyframe-declaration-no-important': true, 
+    'keyframe-declaration-no-important': true,
     // 不允许未知的媒体特性名称
-    'media-feature-name-no-unknown': true, 
+    'media-feature-name-no-unknown': true,
     // @reason 关闭 实际有很多这样用的，且多数人熟悉 css 优先级 【不允许较低特异性的选择器出现在较高特异性选择器之后】
-    'no-descending-specificity': null, 
+    'no-descending-specificity': null,
     // 不允许使用重复的 @import
-    'no-duplicate-at-import-rules': true, 
+    'no-duplicate-at-import-rules': true,
     // 不允许重复的选择器
-    'no-duplicate-selectors': true, 
+    'no-duplicate-selectors': true,
     // 不允许空来源
-    'no-empty-source': null, 
+    'no-empty-source': null,
     // 不允许使用无效的双斜杠注释
-    'no-invalid-double-slash-comments': true, 
+    'no-invalid-double-slash-comments': true,
     // 不允许未知属性
-    'property-no-unknown': true, 
+    'property-no-unknown': true,
     // 不允许未知的伪类
     'selector-pseudo-class-no-unknown': [
       true,
@@ -73,6 +74,9 @@ module.exports = {
     ],
     // 限制选择器中ID选择器的数量
     'selector-max-id': 0,
+
+    'scss/at-rule-no-unknown': true,
+    'scss/double-slash-comment-whitespace-inside': 'always',
   },
   ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
-}
+};
