6fbcabc6d72026a54e4d91947bb3e0fccf771309
huangkaixiang <huangkaixiang@emposat.com>
2025-10-24 11:09:44 +0800

    fix: some bugs

--- packages/lint/eslint-config/tsdown.config.ts
+++ packages/lint/eslint-config/tsdown.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     'typescript/node': './typescript/node.ts',
     formatter: './formatter.ts',
   },
-  dts: true,
+  dts: false,
   shims: true,
   format: ['esm'],
 });
